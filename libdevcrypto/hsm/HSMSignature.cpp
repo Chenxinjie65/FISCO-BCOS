edfd4c5501597636996cbe00f5032f0bdf0b63f2
Chengxinjie65 <1426891084@qq.com>
2025-11-01 16:04:59 +0800

    add

--- libdevcrypto/hsm/HSMSignature.cpp
+++ libdevcrypto/hsm/HSMSignature.cpp
@@ -48,9 +48,9 @@ std::shared_ptr<crypto::Signature> dev::crypto::SDFSM2Sign(
     Key key = Key();
     if (_keyPair.isInternalKey())
     {
-        key = Key((_keyPair.keyIndex() + 1) / 2, std::make_shared<const std::vector<byte>>(std::vector<byte>{'1', '1', '1', '1', '1', '1', '1', '1'}));
-        //CRYPTO_LOG(DEBUG) << "[HSMSignature::key] is internal key "
-        //                  << LOG_KV("keyIndex", key.identifier());
+        key = Key(_keyPair.keyIndex(), std::make_shared<const std::vector<byte>>(std::vector<byte>{'1', '1', '1', '1', '1', '1', '1', '1'}));
+        CRYPTO_LOG(DEBUG) << "[HSMSignature::key] is internal key "
+                          << LOG_KV("keyIndex", key.identifier());
     }
     else
     {
